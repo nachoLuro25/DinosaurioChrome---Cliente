2141619c98f4d0638a39ffeb2eff9ec673649f34
Ignacio Tomas Vildozo <nachotv.25@gmail.com>
2026-02-17 19:48:33 -0300

    Piso Dino ARREGLADO

--- lwjgl3/src/main/java/com/dinosauriojuego/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/dinosauriojuego/lwjgl3/Lwjgl3Launcher.java
@@ -3,7 +3,8 @@ package com.dinosauriojuego.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.*;
 import com.dinosauriojuego.core.Main;
 
-public class Lwjgl3Launcher {
+public class
+Lwjgl3Launcher {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("DinoChrome - Cliente");
